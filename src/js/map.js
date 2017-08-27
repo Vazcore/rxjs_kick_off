1bd88339dc94d44f52563c6e2304e1bfa603c45c
alexgabrusev <alexgabrusev@gmail.com>
2017-08-27 21:52:21 +0300

    Task 2: Working with a real-time data

--- src/js/map.js
+++ src/js/map.js
@@ -3,5 +3,31 @@ import { Observable } from "rxjs";
 import { DOM } from "rx-dom";
 
 
-var map = L.map('map').setView([33.858631, -118.279602], 7);
+var map = L.map('map').setView([32.758631, -117.239602], 9);
 L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png').addTo(map);
+
+const PROXY_URL = 'https://cors-anywhere.herokuapp.com/';
+const API_URL = "http://api.eventful.com/json/events/search?app_key=WJXrJHfPb8NZzXBn&location=San+Diego";
+
+const fetchSubscription = Observable
+.from(fetch(PROXY_URL + API_URL))
+.flatMap(res => Observable.from(res.json()) )
+/**
+ * Task 1: Create Stream of "Moving Map" actions to fetch related events and display it
+*/
+/**
+ * Task 2: Create updates in "Real Time"
+*/
+.flatMap(res => Observable.from(res.events.event))
+.subscribe((event) => {
+  L.circle([event.latitude, event.longitude], 5000).addTo(map);
+})
+
+/**
+ * Task 3: Create Stream of "Hovering to specific Event" actions to display event's details
+*/
+
+
+/**
+ * Task 4: Create a dashboard for adding new Events and diplaying brief list of events
+*/
